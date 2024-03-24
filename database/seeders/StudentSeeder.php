bc030fdd618297fb1576b94ae3c22e98feb5ee3c
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-24 23:53:56 +0800

    Progress: Finish make student seeder

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Student;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Student::create([
+        //     'fullname' => 'I Putu Aditya Prayatna',
+        //     'username' => 'Aditya Prayatna',
+        //     'email' => 'adityaprayatna@gmail.com',
+        //     'date_of_birth' => Carbon::createFromDate(2005, 3, 9)->toDateString(),
+        //     'gender' => 'male',
+        //     'address' => 'Jl. Monang Maning Gg. Gagak',
+        //     'number_phone' => '08123456789',
+        // ]);
+    }
+}
