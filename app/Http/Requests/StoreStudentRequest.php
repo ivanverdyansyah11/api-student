9bbe8b4f427b29efa371f7dd1d3452cd32857d7a
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-24 23:51:45 +0800

    Progress: Finish Store Student Request Validation

---
+++ app/Http/Requests/StoreStudentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'fullname' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'date_of_birth' => 'required|date',
+            'gender' => 'required|string',
+            'address' => 'required',
+            'number_phone' => 'required|string|max:20',
+        ];
+    }
+}
